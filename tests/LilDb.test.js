1f90daa370ccd48e48e3452bc9000a0e73c6ed09
oleg.im@gmail.com <oleg.im@gmail.com>
2024-03-20 23:58:45 -0700

    more unit tests

--- tests/LilDb.test.js
+++ tests/LilDb.test.js
@@ -35,7 +35,7 @@ describe('LilDb', () => {
 
     describe('saveAs', () => {
         it('should write to the file and set the filename', () => {
-            const testData = {data:'test data'};
+            const testData = { data: 'test data' };
             db.insert(testData);
             db.saveAs('newfile.db');
             expect(fs.writeFileSync).toHaveBeenCalledWith('newfile.db', expect.any(String), 'utf8');
@@ -59,7 +59,57 @@ describe('LilDb', () => {
             const result = db.query({ name: 'John' });
             expect(result).toHaveLength(1);
             expect(result[0].name).toBe('John');
-        });
+        })
+
+        it('should return correct query results for nested prop', () => {
+            const data = { name: 'John', age: 30, department: { name: 'Engineering' } };
+            db.insert(data);
+            const result = db.query({ "department.name": 'Engineering' });
+            expect(result).toHaveLength(1);
+            expect(result[0].name).toBe('John');
+        })
+
+        it('should asc sort the result', () => {
+            const data = [
+                { name: 'John', age: 30 },
+                { name: 'Jane', age: 25 }
+            ];
+            db.insert(data);
+            const result = db.query({}, { sortAsc: 'age' });
+            expect(result[0].name).toBe('Jane');
+        })
+
+        it('should asc sort the result', () => {
+            const data = [
+                { name: 'Jane', age: 25 },
+                { name: 'John', age: 30 }
+            ];
+            db.insert(data);
+            const result = db.query({}, { sortDesc: 'age' });
+            expect(result[0].name).toBe('John');
+        })
+
+        it('should group data', () => {
+            const data = [
+                { role: "admin", name: 'Jane', age: 25 },
+                { role: "admin", name: 'Mike', age: 40 },
+                { role: "user", name: 'John', age: 30 }
+            ];
+            db.insert(data);
+            const result = db.query({}, { group: 'role' });
+            expect(result["admin"][0].name).toBe('Jane');
+            expect(result["user"][0].name).toBe('John');
+        })
+
+        it('should use projection', () => {
+            const data = [
+                { name: 'Jane', age: 25 },
+                { name: 'John', age: 30 }
+            ];
+            db.insert(data);
+            const result = db.query({}, { projection: { user: "name" } });
+            expect(result[0].user).toBe('Jane');
+        })
     });
 
     describe('remove', () => {
